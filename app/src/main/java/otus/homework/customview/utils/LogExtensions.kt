d20d9925153cf5c82577f90eb5f5b8536bc7f7c6
bmv-2143 <bmv-2143@yandex.ru>
2024-09-06 13:06:59 +0300

    refactor: simplify tax extension fun

--- app/src/main/java/otus/homework/customview/utils/LogExtensions.kt
+++ app/src/main/java/otus/homework/customview/utils/LogExtensions.kt
@@ -1,8 +1,4 @@
 package otus.homework.customview.utils
 
 val Any.TAG: String
-    get() {
-        val tagMaxAllowedLimit = 23
-        val tag = javaClass.simpleName
-        return if (tag.length <= tagMaxAllowedLimit) tag else tag.substring(0, tagMaxAllowedLimit)
-    }
+    get() = javaClass.simpleName
